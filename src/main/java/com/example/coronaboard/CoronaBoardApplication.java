0fe49f9a42660f7fb12803ef5ee506f3714e2187
MrOdeh <mahmoud.odehgs@gmail.com>
2021-08-16 23:15:00 +0300

    ini

---
+++ src/main/java/com/example/coronaboard/CoronaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.coronaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coronaboard/CoronaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coronaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
